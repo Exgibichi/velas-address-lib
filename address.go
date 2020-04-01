b3ec338cc07525fcc4c60f0443642f8aa1917110
noa <topoyr@gmail.com>
2020-04-01 16:27:37 +0700

    feat: add checksum to golang lib

--- address.go
+++ address.go
@@ -1,54 +1,77 @@
 package main
 
 import (
+	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
-	"math/big"
-	"os"
+	"regexp"
 	"strings"
 
-	"github.com/itchyny/base58-go"
+	"github.com/pkg/errors"
+
+	"github.com/btcsuite/btcutil/base58"
 )
 
-func ethToVlx(address string) (encoded string) {
-	strippedAddressHex := strings.Replace(string(address), "0x", "", 1)
-	decodedHex, err1 := hex.DecodeString(strippedAddressHex)
-	if err1 != nil {
-		fmt.Println(err1.Error())
-		os.Exit(1)
-	}
-	bigInt := new(big.Int).SetBytes(decodedHex)
-	encoding := base58.BitcoinEncoding
-	encodedAddress, err := encoding.Encode([]byte(bigInt.String()))
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-	return "V" + string(encodedAddress)
-}
-
-func vlxToEth(address string) (encoded string) {
-	strippedAddress := strings.Replace(address, "V", "", 1)
-	encoding := base58.BitcoinEncoding
-	decodedAddress, err := encoding.Decode([]byte(strippedAddress))
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-	bigInt, biErr := new(big.Int).SetString(string(decodedAddress), 10)
-	if !biErr {
-		fmt.Println("invalid address can`t convert")
-		os.Exit(1)
-	}
-	hexAddress := fmt.Sprintf("%x", bigInt)
-	return "0x" + hexAddress
-}
-
 func main() {
-	address := "0x32Be343B94f860124dC4fEe278FDCBD38C102D88"
-	encoded := ethToVlx(address)
-	decoded := vlxToEth(encoded)
+	eth := "0x32Be343B94f860124dC4fEe278FDCBD38C102D88"
+	vel := "V5dJeCa7bmkqmZF53TqjRbnB4fG6hxuu4f"
 
-	fmt.Println(encoded)
-	fmt.Println(decoded)
+	encoded, err := EthToVlx(eth)
+	if err != nil {
+		panic(err)
+	}
+
+	decoded, err := VlxToEth(vel)
+	if err != nil {
+		panic(err)
+	}
+
+	if encoded == vel && decoded == strings.ToLower(eth) {
+		fmt.Println("success")
+	}
+}
+
+func EthToVlx(address string) (string, error) {
+	strippedAddressHex := strings.ToLower(strings.TrimPrefix(address, "0x"))
+
+	checksum := sha(sha(strippedAddressHex))[:8]
+	raw := strippedAddressHex + checksum
+
+	dec, err := hex.DecodeString(raw)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to decode long address")
+	}
+
+	result := "V" + base58.Encode(dec)
+	return result, nil
+}
+
+func VlxToEth(address string) (string, error) {
+	clean := strings.TrimPrefix(address, "V")
+	decodedAddress := hex.EncodeToString(base58.Decode(clean))
+
+	regex := regexp.MustCompile(`([0-9abcdef]+)([0-9abcdef]{8})`)
+	if !regex.MatchString(decodedAddress) {
+		return "", errors.New("invalid decoded address")
+	}
+
+	strings := regex.FindStringSubmatch(decodedAddress)
+
+	if len(strings) != 3 {
+		return "", errors.New("invalid address")
+	}
+
+	checksum := sha(sha(strings[1]))[:8]
+
+	if strings[2] != checksum {
+		return "", errors.New("invalid checksum")
+	}
+
+	return "0x" + strings[1], nil
+}
+
+func sha(raw string) string {
+	hasher := sha256.New()
+	hasher.Write([]byte(raw))
+	return hex.EncodeToString(hasher.Sum(nil))
 }
